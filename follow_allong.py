e5661ea9a940969218327e5bf1905191f5d43c49
Christopher Sampah <christopher.m.sampah@gmail.com>
2020-03-29 13:52:13 -0400

    Notes and ideas taken from a kaggle notebook

---
+++ follow_allong.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 12:53:16 2020
+Notes and plotting ideas taken from another Kaggle notebook: 
+    https://www.kaggle.com/pmarcelino/comprehensive-data-exploration-with-python
+
+@author: CSR1
+"""
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from scipy import stats
+
+df_train = pd.read_csv('data/train.csv')
+df_train.set_index('Id', inplace = True)
+
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True);
+
+
+#saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+#scatterplot
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 2.5)
+plt.show();
+
+#histogram and normal probability plot
+sns.distplot(df_train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
+#applying log transformation
+df_train['SalePrice'] = np.log(df_train['SalePrice'])
+#transformed histogram and normal probability plot
+sns.distplot(df_train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
